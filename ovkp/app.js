389610976fd0fcb9cb295f5fdd0374b993cc5d95
hofovea <m.o.zashchyk@gmail.com>
2020-05-31 03:20:52 +0300

    fixed log template | changed corresponding endpoint

--- ovkp/app.js
+++ ovkp/app.js
@@ -10,11 +10,12 @@ const mongoose = require('mongoose');
 const PCCO = require("./models/pcco");
 const testEmailAccount = require("./models/test_email_account");
 const nodemailer = require('nodemailer');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 const dataBaseUrl = 'mongodb://localhost:27017/ovkp';
-const connectOptions = {useNewUrlParser: true};
+const connectOptions = { useNewUrlParser: true };
 
 const viewsDir = path.join(__dirname, 'views');
 app.engine('mst', mustache_expr(path.join(viewsDir, 'partials')));
@@ -32,9 +33,9 @@ app.use(express.static(path.join(__dirname, 'data/fs')));
 app.use(express.static('public'));
 
 mongoose.connect(dataBaseUrl, connectOptions)
-.then(() => console.log(`Database conected: ${dataBaseUrl}`))
-.then(() => app.listen(port, onListen))
-.catch(err => console.log(`Error ${err}`));
+    .then(() => console.log(`Database conected: ${dataBaseUrl}`))
+    .then(() => app.listen(port, onListen))
+    .catch(err => console.log(`Error ${err}`));
 
 function onListen() {
     console.log(`Server running at port ${port}`);
@@ -44,98 +45,98 @@ let curr_user = null;
 
 app.get(`/`, function (req, res) {
     CriminalArticles.getAll()
-    .then(articles => res.render(`main`, {articles: articles, user: curr_user}))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(articles => res.render(`main`, { articles: articles, user: curr_user }))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.post(`/`, function (req, res) {
     CriminalRecord.getAll()
-    .then(records => { return Promise.all([records, CriminalArticles.getAll()]); })
-    .then(([records, articles]) => 
-        res.render(`main`, 
-        {
-            articles: articles,
-            ovkps: search(records, req.body.first_name, req.body.last_name, req.body.surname,
-                req.body.case_number, req.body.article_id),
-            user: curr_user
-        }
-    ))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(records => { return Promise.all([records, CriminalArticles.getAll()]); })
+        .then(([records, articles]) =>
+            res.render(`main`,
+                {
+                    articles: articles,
+                    ovkps: search(records, req.body.first_name, req.body.last_name, req.body.surname,
+                        req.body.case_number, req.body.article_id),
+                    user: curr_user
+                }
+            ))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get(`/articles`, function (req, res) {
     CriminalArticles.getAll()
-    .then(articles => res.render(`articles`, {articles: articles, user: curr_user}))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(articles => res.render(`articles`, { articles: articles, user: curr_user }))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get(`/article_add`, function (req, res) {
-    res.render(`article_add`, {user: curr_user});
+    res.render(`article_add`, { user: curr_user });
 });
 
 app.post('/article_add', function (req, res) {
     let article = new CriminalArticles(req.body.article_number, req.body.article_title, req.body.article_content);
     CriminalArticles.insert(article)
-    .then(() => res.redirect(`/articles`))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(() => res.redirect(`/articles`))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get(`/article_modify/:id`, function (req, res) {
     CriminalArticles.getById(req.params.id)
-    .then(article => res.render(`article_modify`, {article: article, user: curr_user}))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(article => res.render(`article_modify`, { article: article, user: curr_user }))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.post('/article_modify', function (req, res) {
     const aricleId = req.body.aricleId;
     CriminalArticles.getById(aricleId)
-    .then(article => {
-    article.article_number = req.body.article_number;
-    article.article_title = req.body.article_title;
-    article.article_content = req.body.article_content;
-    return CriminalArticles.update(article);
-    })
-    .then(res.redirect(`/articles`))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(article => {
+            article.article_number = req.body.article_number;
+            article.article_title = req.body.article_title;
+            article.article_content = req.body.article_content;
+            return CriminalArticles.update(article);
+        })
+        .then(res.redirect(`/articles`))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get(`/criminal_record`, function (req, res) { // todo
 
-    res.render(`criminal_record`, {user: curr_user});
+    res.render(`criminal_record`, { user: curr_user });
 });
 
 app.get(`/criminal_record_add`, function (req, res) { // todo
     PCCO.getAll()
-    .then(pccos => res.render(criminal_record_add, {pccos: pccos, user: curr_user}))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(pccos => res.render(criminal_record_add, { pccos: pccos, user: curr_user }))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.post(`/criminal_record_add`, function (req, res) { // todo ебучие логи
-    res.render(`criminal_record_add`, {user: curr_user});
+    res.render(`criminal_record_add`, { user: curr_user });
 });
 
 app.get(`/criminal_record_modify/:id`, function (req, res) { // todo
 
-    res.render(`criminal_record_modify`, {user: curr_user});
+    res.render(`criminal_record_modify`, { user: curr_user });
 });
 
 app.post(`/criminal_record_modify`, function (req, res) { // todo ебучие логи
-    res.render(`criminal_record_modify`, {user: curr_user});
+    res.render(`criminal_record_modify`, { user: curr_user });
 });
 
 app.get(`/login`, function (req, res) {
-    res.render(`login`, {user: curr_user});
+    res.render(`login`, { user: curr_user });
 });
 
 app.post(`/login`, function (req, res) {
     User.getByEmailAndPassword(req.body.email, req.body.password)
-    .then(user => {
-        curr_user = user;
-        if (!curr_user)
-            res.redirect('/login');
-        res.redirect('/');
-    })
-    .catch(err => res.status(500).send(err.toString()));
+        .then(user => {
+            curr_user = user;
+            if (!curr_user)
+                res.redirect('/login');
+            res.redirect('/');
+        })
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get(`/logout`, function (req, res) {
@@ -144,41 +145,43 @@ app.get(`/logout`, function (req, res) {
 });
 
 app.get(`/logs`, function (req, res) { // todo
-
-    res.render(`logs`, {user: curr_user});
+    const log_obj = JSON.parse(fs.readFileSync('./data/logs.json', 'utf8'));
+    res.render(`logs`, { logs: JSON.stringify(log_obj.logs, null, 4), 
+                        created_at: JSON.stringify(log_obj.logs[0].created_at, null, 4),
+                        new_values: JSON.stringify(log_obj.logs[0].new_values, null, 4) });
 });
 
 app.get(`/ovkp_add`, function (req, res) { // todo
-    res.render(`ovkp_add`, {user: curr_user});
+    res.render(`ovkp_add`, { user: curr_user });
 });
 
 app.post(`/ovkp_add`, function (req, res) { // todo
-    res.render(`ovkp_add`, {user: curr_user});
+    res.render(`ovkp_add`, { user: curr_user });
 });
 
 app.get(`/ovkp_modify/:id`, function (req, res) { // todo
-    res.render(`ovkp_modify`, {user: curr_user});
+    res.render(`ovkp_modify`, { user: curr_user });
 });
 
 app.get(`/ovkp_modify`, function (req, res) { // todo
-    res.render(`ovkp_modify`, {user: curr_user});
+    res.render(`ovkp_modify`, { user: curr_user });
 });
 
 app.get(`/registrars`, function (req, res) {
     User.getRegistrars()
-    .then(registrars => res.render(`registrars`, {registrars: registrars, user: curr_user}))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(registrars => res.render(`registrars`, { registrars: registrars, user: curr_user }))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get(`/registrars_add`, function (req, res) {
-    res.render(`registrars_add`, {user: curr_user});
+    res.render(`registrars_add`, { user: curr_user });
 });
 
 app.post('/registrars_add', function (req, res) {
     let user = new User(req.body.full_name, 1, req.body.email, req.body.password);
     User.insert(user)
-    .then(() => res.redirect(`/registrars`))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(() => res.redirect(`/registrars`))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 function search(allCriminalRecords, first_name, last_name, surname, court_case_number, criminal_article_id) {
@@ -211,7 +214,7 @@ function search(allCriminalRecords, first_name, last_name, surname, court_case_n
         }
     }
     else filteredByLastName = Array.from(filteredByFirstName);
-    
+
     let filteredBySurname = [];
     if (surname != '') {
         for (let record of filteredByLastName) {
@@ -248,35 +251,35 @@ let transporter = nodemailer.createTransport({
 app.get('/activate/:id', function (req, res) {
     const userId = req.params.id;
     User.getById(userId)
-    .then(user => {
-        user.is_active = true;
-    return Promise.all([user, User.update(user)]);
-    })
-    .then(([user]) => {
-        return transporter.sendMail({
-            from: '"Anti-corruption Main Office" <ivannyakovlev99@gmail.com>',
-            to: user.email,
-            subject: "Активація акаунту",
-            html: `<p style="font-family: Trebuchet MS, sans-serif; font-size: 12pt;">Вітаємо, <strong style="font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 14pt;">${user.full_name}</strong></p>` +
-            '<p style="font-family: Trebuchet MS, sans-serif; font-size: 12pt;">Ваш акаунт було успішно активовано!</p>' +
-            '<p style="font-family: Andale Mono, monospace; font-size: 12pt;">Ваші дані для авторизації в системі:</p>' +
-            `<p style="font-family: Andale Mono, monospace; font-size: 10pt;"><i><b>Логін:</b></i> ${user.email}</p>` +
-            `<p style="font-family: Andale Mono, monospace; font-size: 10pt;"><i><b>Пароль:</b></i> ${user.password}</p>`
-        });
-    })
-    .then(() => res.redirect(`/registrars`))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(user => {
+            user.is_active = true;
+            return Promise.all([user, User.update(user)]);
+        })
+        .then(([user]) => {
+            return transporter.sendMail({
+                from: '"Anti-corruption Main Office" <ivannyakovlev99@gmail.com>',
+                to: user.email,
+                subject: "Активація акаунту",
+                html: `<p style="font-family: Trebuchet MS, sans-serif; font-size: 12pt;">Вітаємо, <strong style="font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 14pt;">${user.full_name}</strong></p>` +
+                    '<p style="font-family: Trebuchet MS, sans-serif; font-size: 12pt;">Ваш акаунт було успішно активовано!</p>' +
+                    '<p style="font-family: Andale Mono, monospace; font-size: 12pt;">Ваші дані для авторизації в системі:</p>' +
+                    `<p style="font-family: Andale Mono, monospace; font-size: 10pt;"><i><b>Логін:</b></i> ${user.email}</p>` +
+                    `<p style="font-family: Andale Mono, monospace; font-size: 10pt;"><i><b>Пароль:</b></i> ${user.password}</p>`
+            });
+        })
+        .then(() => res.redirect(`/registrars`))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get('/deactivate/:id', function (req, res) {
     const userId = req.params.id;
     User.getById(userId)
-    .then(user => {
-        user.is_active = false;
-    return User.update(user);
-    })
-    .then(res.redirect(`/registrars`))
-    .catch(err => res.status(500).send(err.toString()));
+        .then(user => {
+            user.is_active = false;
+            return User.update(user);
+        })
+        .then(res.redirect(`/registrars`))
+        .catch(err => res.status(500).send(err.toString()));
 });
 
 app.get('*', (req, res) => {
